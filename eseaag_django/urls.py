55856d0de760a251977e762df5ae01e68d767312
Brandon-Schefstad <bschefstad@gmail.com>
2023-08-13 22:52:15 -0400

    Uploading for work

--- eseaag_django/urls.py
+++ eseaag_django/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from student_manager.views import accommodations
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accommodations/', accommodations, name="accommodations")
 ]
--- student_manager/views.py
+++ student_manager/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from .models import Accommodation
 # Create your views here.
+def accommodations(request):
+    accommodations = Accommodation.objects.all()
+    return render(request, 'accommodations.html', {"accommodations":accommodations})
